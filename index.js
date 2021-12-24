25740da3a63a29296e8160fd62c6ea68decaae0d
Suriyasiva <93177461+Suriyasiva@users.noreply.github.com>
2021-12-23 23:04:39 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+const scores = [
+    {
+      marks: 32,
+      name: "Yvette Merritt"
+    },
+    {
+      marks: 57,
+      name: "Lillian Ellis"
+    },
+    {
+      marks: 22,
+      name: "Mccall Carter"
+    },
+    {
+      marks: 21,
+      name: "Pate Collier"
+    },
+    {
+      marks: 91,
+      name: "Debra Beard"
+    },
+    {
+      marks: 75,
+      name: "Nettie Hancock"
+    },
+    {
+      marks: 20,
+      name: "Hatfield Hodge"
+    }
+  ];
+// task 1
+var names=(element)=>element.name
+var res=scores.map(names)
+console.log(res);
+
+//task2 
+var names=(element)=>element.marks>=40
+var res=scores.filter(names)
+console.log(res);
+
+// task3
+var names=(element)=>element.marks<=40
+var res=scores.filter(names)
+for(i=0;i<res.length;i++){
+console.log(res[i].name);
+}
+
+
+  // [32, 57, 22]
+  // Task 1
+  // Print the name list - As an array
+  // Expected Output
+  // ["Yvette Merritt", "Lillian Ellis", "Mccall Carter", "Pate Collier" ,
+  //  "Debra Beard", "Nettie Hancock",  "Hatfield Hodge"]
+  
+  // Task 2
+  // >=40 pass.. find those student that passed (Use array method)
+  // Expected Output
+  // [{
+  //     marks: 57,
+  //     name: "Lillian Ellis"
+  //   },
+  //     {
+  //     marks: 91,
+  //     name: "Debra Beard"
+  //   },
+  //   {
+  //     marks: 75,
+  //     name: "Nettie Hancock"
+  //   }]
+  
+  // Task 3
+  //  Find all the names who failed the exams (Array methods)
+  // Expected Output
+  //   ["Yvette Merritt",
+  //   "Mccall Carter",
+  //   "Pate Collier",
+  //   "Hatfield Hodge"
+  // ];
+  
